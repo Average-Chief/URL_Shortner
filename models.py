0285c8cfc750346620096463408d146a7fa06591
Average-Chief <achalsinghtomar3450@gmail.com>
2026-01-18 16:19:02 +0530

    Add database connection setup in models.py

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, create_engine
 from typing import Optional
 from datetime import timedelta, datetime
+
+DATABASE_URL = "sqlite:///test.db"
+engine = create_engine(DATABASE_URL, echo=True)
 
 class Url(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -9,3 +12,6 @@ class Url(SQLModel, table=True):
     access_count: int = Field(default=0, nullable=False)
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
